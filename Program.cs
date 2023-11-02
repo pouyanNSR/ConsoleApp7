609fbf883c8d10ea098ef4003528c912c6c7a8a6
Nasiri-PC <Nasiri-PC@DESKTOP-0LN6NE5>
2023-11-02 20:15:06 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("your average number is: ");
+int [] mark = new int [10];
+{
+    mark[0] = 17;
+    mark[1] = 18;
+    mark[2] = 20;
+    mark[3] = 13;
+    mark[4] = 15;
+    mark[5] = 17;
+    mark[6] = 20;
+    mark[7] = 19;
+    mark[8] = 14;
+    mark[9] = 17;
+}
+ double avg = (mark[0] + mark[1] + mark[2] + mark[3] + mark[4] + mark[5] + mark[6] + mark[7] + mark[8] + mark[9]) / (10);
+
+Console.WriteLine(avg);
+
+// پویان نصیری  تمرین شماره 3
+
+
+
+
